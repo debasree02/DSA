de3b8ed041cc61596dc3f81f7686d8b120397965
1023434 <1023434@IN2-1023434LT.jda.corp.local>
2019-10-29 14:14:20 +0530

    day 3

---
+++ test/src/test/testclass1.java
@@ -0,0 +1,19 @@
+package test;
+
+public class testclass1 {
+
+	public static void testclass1() {
+		int a=2;
+		int b=4;
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		testclass1();
+		
+
+	}
+
+	
+}
